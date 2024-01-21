82f8c35fff85a03e978324b3edfcede788ae9300
Al Tsaplin <al.tsaplin@gmail.com>
2024-01-22 02:54:31 +0300

    Half-done (Hero section) Headings and description done. Video pop-up left.

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+// GLIGHTBOX SETTINGS
+// https://www.npmjs.com/package/glightbox
+const lightbox = GLightbox({
+  elements: [{
+    'href': 'https://www.youtube.com/watch?v=J9lS14nM1xg',
+    'type': 'video',
+    'source': 'youtube', //vimeo, youtube or local
+    'width': 900,
+  }],
+  autoplayVideos: true,
+});
+
+lightbox.open();
