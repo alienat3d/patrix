e21badef27cf6c95c17efc8190371995848f3964
Al Tsaplin <al.tsaplin@gmail.com>
2024-01-26 07:54:51 +0300

    Add (Section "hero") Also Glightbox JS Plugin for video popup

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,9 @@
 // GLIGHTBOX SETTINGS
 // https://www.npmjs.com/package/glightbox
 const lightbox = GLightbox({
-  elements: [{
-    'href': 'https://www.youtube.com/watch?v=J9lS14nM1xg',
-    'type': 'video',
-    'source': 'youtube', //vimeo, youtube or local
-    'width': 900,
-  }],
+  'href': 'https://www.youtube.com/watch?v=J9lS14nM1xg',
+  'type': 'video',
+  'source': 'youtube', //vimeo, youtube or local
+  'width': 900,
   autoplayVideos: true,
 });
-
-lightbox.open();
